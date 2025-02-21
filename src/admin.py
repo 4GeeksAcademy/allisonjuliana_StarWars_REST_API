9d9875794d8d73661698e7587bd3f36693979866
allisonjuliana <162182668+allisonjuliana@users.noreply.github.com>
2025-02-21 17:28:44 +0000

    entregando

--- src/admin.py
+++ src/admin.py
@@ -13,3 +13,4 @@ def setup_admin(app):
     admin.add_view(ModelView(Characters, db.session))
     admin.add_view(ModelView(Planets, db.session))
     admin.add_view(ModelView(Favorites, db.session))
+    
